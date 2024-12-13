2beeb33971febe54fcb918366c82d2cceea446fa
Vlad-Alexandru Dindeal <vlladin@gmail.com>
2024-12-13 14:02:16 +0200

    Added the beacon chain accounting

--- src/delegated_validators_registry.py
+++ src/delegated_validators_registry.py
@@ -1,4 +1,5 @@
 from typing import List
+from validator import Validator
 from custom_types import Gwei, ValidatorIndex, BLSPubkey
 from delegated_validator import DelegatedValidator
 
@@ -11,6 +12,19 @@ class DelegatedValidatorsRegistry:
     def process_delegation(self, delegator_index: ValidatorIndex, validator_id: BLSPubkey, amount: Gwei):
         delegated_validator = self._get_delegated_validator_by_id(validator_id)
         delegated_validator.increase_balance(delegator_index, amount)
+
+    def create_delegated_validator(self, validator: Validator, initial_balance: Gwei):
+        delegated_validator = DelegatedValidator(validator, initial_balance)
+        self.delegated_validators.append(delegated_validator)
+
+    def is_validator_delegated(self, validator_id: BLSPubkey):
+        is_delegated = False
+        for dv in self.delegated_validators:
+            if dv.delegated_validator.validator_id == validator_id:
+                is_delegated = True
+                break
+
+        return is_delegated
         
     def _get_delegated_validator_by_id(self, validator_id: BLSPubkey):
         delegated_validator = None
--- src/main.py
+++ src/main.py
@@ -1,8 +1,4 @@
-from delegated_validator import DelegatedValidator
-from validator import Validator
+from beacon_chain_accounting import BeaconChainAccounting
 
-dv = DelegatedValidator(Validator("#123"), 10)
-
-dv.increase_balance(0, 5)
-dv.increase_balance(5, 5)
-dv.increase_balance(5, 5)
+beacon_chain_accounting = BeaconChainAccounting()
+beacon_chain_accounting.delegate()
