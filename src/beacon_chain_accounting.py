96cc9cfb4b71a25f19cf95ce89985b3fa0d1741f
Vlad-Alexandru Dindeal <vlladin@gmail.com>
2024-12-13 14:18:27 +0200

    Prepared for dummy data

--- src/beacon_chain_accounting.py
+++ src/beacon_chain_accounting.py
@@ -1,14 +1,16 @@
 from delegated_validators_registry import DelegatedValidatorsRegistry
 from custom_types import BLSPubkey, Gwei, DelegatorIndex
 from validator import Validator
+from delegators_registry import DelegatorsRegistry
 
 
 class BeaconChainAccounting:
+    delegators_registry: DelegatorsRegistry
     delegated_validators_registry: DelegatedValidatorsRegistry
-
+    
     def __init__(self):
         self.delegated_validators_registry = DelegatedValidatorsRegistry()
-        pass
+        self.delegators_registry = DelegatorsRegistry()
 
     def delegate(self, delegator_index: DelegatorIndex, validator_id: BLSPubkey, amount: Gwei):
         validator = 1 # get it from the registry
@@ -16,3 +18,8 @@ class BeaconChainAccounting:
             self.delegated_validators_registry.create_delegated_validator(validator, amount)
 
         self.delegated_validators_registry.process_delegation(delegator_index, validator.validator_id, amount)    
+
+    def test_generate_test_data(self):
+        print("Data generated")
+        
+        
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from beacon_chain_accounting import BeaconChainAccounting
 
 beacon_chain_accounting = BeaconChainAccounting()
-beacon_chain_accounting.delegate()
+beacon_chain_accounting.test_generate_test_data()
+
+beacon_chain_accounting.delegate(0, )
