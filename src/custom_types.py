75adee4d3ab6ea56d74bf12dcae33545070052a8
gorondan <goron.dan@gmail.com>
2024-12-13 12:05:46 +0200

    correct type deffinintion

--- src/custom_types.py
+++ src/custom_types.py
@@ -1,14 +1,9 @@
 import numpy as np
 
-# State list lengths
-VALIDATOR_REGISTRY_LIMIT = 100  # Validator registry size limit
-DELEGATOR_REGISTRY_LIMIT = 100  # Delegator registry size limit
-
-
 # Custom types
-Gwei = np.uint64
-Epoch = np.uint64
-Fee =  np.uint
-Quota =  np.uint
-delegator_index = np.uint
-validator_index = np.uint
+Gwei : np.uint64
+Epoch : np.uint64
+Fee :  np.uint
+Quota :  np.uint
+delegator_index : np.uint
+validator_index : np.uint
