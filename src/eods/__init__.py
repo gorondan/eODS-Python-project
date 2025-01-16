fe9326a2e3c003ec1fc93ef949b95a0932ed9b82
gorondan <goron.dan@gmail.com>
2025-01-16 12:01:34 +0200

    fixed typo

--- src/eods/__init__.py
+++ src/eods/__init__.py
@@ -1,5 +1,5 @@
 """
-This is the core module of the project, holding the basics of the logic behind ODS.
+This is the core module of the project, holding the basics of the logic behind eODS.
 It containins vital sub-modules like the registry of validators, the registry of delegators, 
 or the delegated validator and delegator classes.
 """
